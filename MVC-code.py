ea5f63ed20745d90a6f22b0a3ad5f79ddf452883
Zach <zquinn1@binghamton.edu>
2017-05-02 08:33:20 -0400

    Revised MVC code

--- MVC-code.py
+++ MVC-code.py
@@ -10,12 +10,17 @@ class Paddle(pygame.sprite.Sprite):
         self.paddleWidth = 100
         self.paddleHeight = 20
 
-        self.ballVelocity = 0
-        self.paddleLeftChange = 0
+        self.speed = 0
+        self.paddle_x_change = 0
         self.key_press = 0
 
         self.screenWidth = 640
         self.screenHeight = 400
+
+        self.image = pygame.Surface([self.paddleWidth, self.paddleHeight])
+        self.rect = self.image.get_rect()
+        self.rect.x = 320
+        self.rect.y = 440
 
     def movePaddle(self, key_press):
         """x = 1 is the left arrow
@@ -23,16 +28,16 @@ class Paddle(pygame.sprite.Sprite):
         x = 3 is the spacebar"""
 
         if self.key_press == 0:
-            self.paddleLeftChange = 0
-        if self.key_press == 1 and self.ballVelocity != 0:
-            self.paddleLeftChange = -5
+            self.paddle_x_change = 0
+        if self.key_press == 1 and self.speed != 0:
+            self.paddle_x_change = -5
         elif self.key_press == 1 and self.paddleLeft <= 0:
-            self.paddleLeftChange = 0
+            self.paddle_x_change = 0
 
-        elif (key_press == 2) and self.ballVelocity != 0:
-            self.paddleLeftChange = 5
+        elif (key_press == 2) and self.speed != 0:
+            self.paddle_x_change = 5
         elif key_press == 2 and self.paddleLeft >= 580:
-            self.paddleLeftChange = 580
+            self.paddle_x_change = 580
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
@@ -40,37 +45,43 @@ class Ball(pygame.sprite.Sprite):
         self.p = Paddle()
         self.ballRadius = 8
         self.ballPos = [342, 433]
-        self.ballVelocity = 8.0 #MIGHT NOT NEED
+        self.speed = 8.0 #MIGHT NOT NEED
+        self.angle = 180
         self.screenWidth = 640
+
+        self.image = pygame.Surface([self.ballRadius, self.ballRadius])
+        self.rect = self.image.get_rect()
 
     def rebound(self, value):
         """REBOUNDS OR BOUNCES BALL OF OF WALL"""
         self.angle = (180 - self.angle) % 360
         self.angle -= value
 
-    def update(self):
+    def update(self, xcomp, ycomp):
         """BALL LOCATION UPDATE"""
-        self.diectionRad = math.radians(self.angle)#CONVERTS DIRECTION ANGLE TO RADIANS
+        self.directionRad = math.radians(self.angle) #CONVERTS DIRECTION ANGLE TO RADIANS
+        self.xcomp = xcomp
+        self.ycomp = ycomp
 
         """ANGLE RADIAN CONVERSION TO SIN AND COS"""
-        self.x += self.speed * math.sin(directionRad)
-        self.y -+ self.speed * math.cos(directionRad)
+        self.xcomp += self.speed * math.sin(self.directionRad)
+        self.ycomp -+ self.speed * math.cos(self.directionRad)
 
         """CHANGES X AND Y OF BALL COORDINATE"""
-        self.ballPos[0] = self.x
-        self.ballPos[1] = self.y
+        self.rect.x = self.xcomp
+        self.rect.y = self.ycomp
 
         """IF STATEMENTS FOR BALL REBOUND"""
-        if self.y <= 0:
+        if self.ycomp <= 0:
             self.rebound(0)
-            self.y = 1
-        if self.x <= 0:
+            self.ycomp = 1
+        if self.xcomp <= 0:
             self.angle = (360 - self.angle)
-            self.x = 1
-        if self.x > (self.screenWidth - self.ballRadius):
+            self.xcomp = 1
+        if self.xcomp > (self.screenWidth - self.ballRadius):
             self.angle = (360 - self.angle)
-            self.x = (self.screenWidth - self.ballRadius) - 1
-        if self.y > 600:
+            self.xcomp = (self.screenWidth - self.ballRadius) - 1
+        if self.ycomp > 600:
             return True
         else:
             return False
@@ -81,12 +92,14 @@ class Brick(pygame.sprite.Sprite):
     width = 48
     height = 10
 
-    def __init__(self, xcor, ycor):
+    def __init__(self, color, xcor, ycor):
         super().__init__()
         """INITIALIZES BLOCK COLOR AND COORDINATES"""
-        # self.brickObj = pygame.draw.rect(gameDisplay, color, Brick.width, Brick.height)
-        # self.brickObj.fill(color)
-        # self.rect = self.image.get_rect()
+        self.image = pygame.Surface([Brick.width, Brick.height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = xcor
+        self.rect.y = ycor
 
 
     @classmethod
@@ -102,117 +115,103 @@ class Brick(pygame.sprite.Sprite):
 
 class Controller(pygame.sprite.Sprite):
 
-        def __init__(self):
-            super().__init__()
-            """PYGAME INITIALIZATION"""
-            pygame.init()
+    def __init__(self):
+        super().__init__()
+        """PYGAME INITIALIZATION"""
+        pygame.init()
 
-            """CLASS INSTANCES"""
-            self.b = Ball()
-            self.p = Paddle()
+        """COLORS"""
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+        self.greenish = (0, 50, 50)
+        self.blueish = (0, 255, 204)
 
-            """WINDOW DIMENSIONS"""
-            self.displayDimensions = (640,480)
-            self.gameDisplay = pygame.display.set_mode(self.displayDimensions)
-            pygame.display.set_caption('Brick Ball!')
+        """CLASS INSTANCES"""
+        self.b = Ball()
+        self.p = Paddle()
+        self.brick = Brick(self.white, 0,0)
 
-            """COLORS"""
-            self.white = (255, 255, 255)
-            self.black = (0, 0, 0)
-            self.greenish = (0, 50, 50)
-            self.blueish = (0, 255, 204)
+        """WINDOW DIMENSIONS"""
+        self.displayDimensions = (640,480)
+        self.gameDisplay = pygame.display.set_mode(self.displayDimensions)
+        pygame.display.set_caption('Brick Ball!')
 
-            """OBJECTS"""
-            self.paddleObj = pygame.Surface([self.p.paddleWidth, self.p.paddleHeight])
-            self.ballObj = pygame.Surface([self.b.ballRadius, self.b.ballRadius])
-            self.brickObj = pygame.Surface([48, 10])
+        """SPRITE GROUPS"""
+        self.brick.bricks = pygame.sprite.Group()
+        self.ballGroup = pygame.sprite.Group()
+        self.allSprites = pygame.sprite.Group()
 
-            self.paddleRect = self.paddleObj.get_rect()
-            self.ballRect = self.ballObj.get_rect()
-            self.brickRect = self.brickObj.get_rect()
+        self.allSprites.add(self.p)
 
-            brick_xcor = self.brickRect.x
-            brick_ycor = self.brickRect.y
-
-            self.brick = Brick(brick_xcor, brick_ycor)
+        self.ballGroup.add(self.b)
+        self.allSprites.add(self.b)
 
 
+        """BLOCK CREATION"""
+        topBrick = 80
+        bricksNum = 15
+        self.brick.build_bricks(self.brick.bricks, bricksNum, topBrick, self.allSprites)
 
 
-            """SPRITE GROUPS"""
-            self.brick.bricks = pygame.sprite.Group()
-            self.ballGroup = pygame.sprite.Group()
-            self.allSprites = pygame.sprite.Group()
+        """FONT"""
+        typicalFont = pygame.font.SysFont('Helvetica', 80, bold = True)
 
-            self.allSprites.add(self.p)
+    def game(self):
+        clock = pygame.time.Clock()
 
-            self.allSprites.add(self.b)
-            self.ballGroup.add(self.b)
+        gameExit = False
+        key_press = 0
 
-            """BLOCK CREATION"""
-            topBrick = 80
-            bricksNum = 15
-            self.brick.build_bricks(self.brick.bricks, bricksNum, topBrick, self.allSprites)
-
-            """FONT"""
-            typicalFont = pygame.font.SysFont('Helvetica', 80, bold = True)
-
-        def game(self):
-            clock = pygame.time.Clock()
-
-            gameExit = False
-            key_press = 0
-
-            """GAME LOOP"""
-            while not gameExit:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        gameExit = True
+        """GAME LOOP"""
+        while not gameExit:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    gameExit = True
 
                 """PADDLE CONTROL"""
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_LEFT and self.ballVelocity != [0,0]:
-                        p.key_press = 1
-                        p.movePaddle(x)
-                    elif event.key == pygame.K_RIGHT and self.ballVelocity != [0,0]:
-                        p.key_press = 2
-                        p.movePaddle(x)
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_SPACE or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                        p.key_press = 0
-                        p.movePaddle(x)
+                    if event.key == pygame.K_LEFT and self.b.speed == 0:
+                        x = 1
+                        self.p.movePaddle(x)
+                    elif event.key == pygame.K_RIGHT and self.b.speed == 0:
+                        x = 2
+                        self.p.movePaddle(x)
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_SPACE or event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                            x = 0
+                            self.p.movePaddle(x)
 
                 if not gameExit:
-                    b.ballUpdate()
+                    self.b.update(self.b.rect.x, self.b.rect.y)
 
                 """COLLISION HANDLING"""
-                if pygame.sprite.spritecollide(p, ballGroup, False):
-                    control = (self.paddleRect.x + p.paddleWidth / 2) - (b.ballPos[0] + b.ballRadius / 2)
-                    b.ballPos[1] = gameDisplay.get_height() - p.paddleHeight - b.ballRadius - 1
-                    b.rebound(control)
+                if pygame.sprite.spritecollide(self.p, self.ballGroup, False):
+                    control = (self.paddleRect.x + self.p.paddleWidth / 2) - (self.b.ballRect + self.b.ballRadius / 2)
+                    self.b.ballPos[1] = self.gameDisplay.get_height() - self.p.paddleHeight - self.b.ballRadius - 1
+                    self.b.rebound(self.control)
 
-                # hitBlocks = pygame.sprite.spritecollide(self.b, self.brick, True)
-                # if len(self.brick) > 0:
-                #     ball.rebound(0)
-                # if len(self.bricks) == 0:
-                #     gameExit = True
-                #     winmsg = typicalFont.render('Winner Winner Chicken Dinner', 0, white)
-                #     winmsgpos = winmsgpos.get_rect(centerx = 400)
-                #     winmsgBottom = 300
-                #     screen.blit(winmsg, winmsgPos)
-                # allSprites.draw(screen)
+                hitBlocks = pygame.sprite.spritecollide(self.b, self.brick.bricks, True)
+                if len(self.brick.bricks) > 0:
+                    self.b.rebound(0)
+                if len(self.brick.bricks) == 0:
+                    gameExit = True
+                    winmsg = typicalFont.render('Winner Winner Chicken Dinner', 0, white)
+                    winmsgpos = winmsgpos.get_rect(centerx = 400)
+                    winmsgBottom = 300
+                    screen.blit(winmsg, winmsgPos)
 
-            gameDisply.fill(greenish)
+            self.gameDisplay.fill(self.greenish)
 
-            p.paddleLeft += (p.paddleLeftChange)
-            paddleImage = pygame.Surface([p.paddleWidth, p.paddleHeight])
-            paddleImage.fill(white)
+            self.allSprites.draw(self.gameDisplay)
+            self.ballGroup.draw(self.gameDisplay)
+
+            self.p.rect.x += (self.p.paddle_x_change)
 
             pygame.display.update()
             clock.tick(60)
 
-            pygame.quit
-            quit()
+        pygame.quit
+        quit()
 
 if __name__ == '__main__':
     run = Controller()
